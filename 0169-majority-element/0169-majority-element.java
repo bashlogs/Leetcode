76e03148c0193dd7de8bf0b998df9c2d493a716a
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2023-07-29 11:02:37 +0530

    Time: 1 ms (99.83%), Space: 48.5 MB (58.73%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    //Boyer-Moore Voting Algorithm
+    public int majorityElement(int[] nums) {
+        int max = nums[0];
+        int count = 1;
+        for(int i=1; i<nums.length; i++){
+            if(count == 0){
+                max = nums[i];
+                count = 1;
+            }
+            else if(max == nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return max;
+    }
+}
