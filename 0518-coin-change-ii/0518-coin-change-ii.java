60bbfff2f5d190c4872ccb7bd2adbfa15fc152e4
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2023-08-13 17:33:32 +0530

    Time: 2 ms (100%), Space: 40 MB (82.11%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,25 +1,12 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[coins.length + 1][amount+1];
-        for(int i=0; i<=coins.length; i++){
-            dp[i][0] = 1;
-        }
-        for(int i=1; i<=amount; i++){
-            dp[0][i] = 0;
-        }
-
-        for(int i=1; i<=coins.length; i++){
-            for(int j=1; j<=amount; j++){
-                if(j>=coins[i-1]){
-                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
-                System.out.print(dp[i][j]+" ");
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for(int coin : coins){
+            for(int i = coin; i<=amount; i++){
+                dp[i] += dp[i-coin];
             }
-            System.out.println();
         }
-        return dp[coins.length][amount];
+        return dp[amount];
     }
 }
