c453899a5dbc278746a5167cbd6ca0a363d08248
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2023-08-14 14:31:23 +0530

    Time: 4 ms (95.5%), Space: 44.6 MB (22.51%) - LeetHub

--- 1850-minimum-length-of-string-after-deleting-similar-ends/1850-minimum-length-of-string-after-deleting-similar-ends.java
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/1850-minimum-length-of-string-after-deleting-similar-ends.java
@@ -1,38 +1,21 @@
 class Solution {
     public int minimumLength(String s) {
-        int a = 0;
-        int j = s.length()-1;
+        int left = 0;
+        int right = s.length()-1;
 
-        while(a < j){
-            if(s.charAt(a) == s.charAt(j)){
-                a++;
-                j--;
-                while(a<=j && s.charAt(a) == s.charAt(a-1)){
-                    a++;
-                }
-                while(a<=j && s.charAt(j) == s.charAt(j+1)){
-                    j--;
-                }
-            }
-            else{
-                break;
-            }
-        }
+        while(s.charAt(left) == s.charAt(right) && left < right )
+        {
+            char target = s.charAt(left);
 
-        
-        System.out.println(a+" "+j);
-        if(a == j){
-            return 1;
-        }
-        else if(a > j){
-            return 0;
-        }
-        else{
-            String new1 = new String();
-            for(int i=a; i<=j; i++){
-                new1 += s.charAt(a);
+            while(s.charAt(left) == target && left < right) {
+                left++;
             }
-            return new1.length();
+
+            while(s.charAt(right) == target && left <= right) {
+                right--;
+            }
+
         }
+        return right - left + 1;
     }
 }
