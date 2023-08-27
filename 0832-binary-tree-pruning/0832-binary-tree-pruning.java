6bb7f1df491c09c208acae2c965eb329226101dc
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2023-08-27 12:29:38 +0530

    Time: 4 ms (100%), Space: 40.4 MB (33.59%) - LeetHub

---
+++ 0832-binary-tree-pruning/0832-binary-tree-pruning.java
@@ -0,0 +1,18 @@
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        
+        if(root.left != null || root.right != null){
+            System.out.println(root.val);
+            root.left = pruneTree(root.left);
+            root.right = pruneTree(root.right);
+        }
+
+        if(root.val == 0 && root.left == null && root.right == null){
+            return null;
+        }
+        return root;
+    }
+}
