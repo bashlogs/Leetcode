9d542695d016fee9e40e5c21e265177512e2905b
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2023-09-01 22:34:22 +0530

    Time: 2 ms (73.73%), Space: 46.6 MB (37.75%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int count(String num){
-        int count = 0;
-        for(int i=0;i<num.length(); i++){
-            if(num.charAt(i) == '1'){
-                count++;
-            }
-        }
-        return count;
-    }
     public int[] countBits(int n) {
-        int[] count = new int[n+1];
-        for(int i=0; i<=n; i++){
-            count[i] = count(Integer.toBinaryString(i));
+        int sol[]=new int[n+1];
+        sol[0]=0;
+        for(int i=1;i<=n;i++){
+            sol[i]=numOnes(i);
+        }
+        return sol;
+    }
+    private int numOnes(int n){
+        int count=0;
+        while(n!=0){
+            count+=(n&1);
+            n>>>=1;
         }
         return count;
     }
