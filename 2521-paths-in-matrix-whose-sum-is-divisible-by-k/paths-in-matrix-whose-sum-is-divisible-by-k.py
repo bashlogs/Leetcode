7ce08d6ad234b955ba69d6abfb98120814881030
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2025-11-26 23:08:43 +0530

    Time: 2065 ms (38.57%) | Memory: 98.4 MB (68.16%) - LeetSync

---
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numberOfPaths(self, grid: List[List[int]], k: int) -> int:
+        MOD = 10**9 + 7
+        m, n = len(grid), len(grid[0])
+
+        dp = [[[0] * k for _ in range(n)] for _ in range(m)]
+
+        dp[0][0][grid[0][0] % k] = 1
+
+        for i in range(m):
+            for j in range(n):
+                for r in range(k):
+                    if i == 0 and j == 0:
+                        continue
+
+                    val = grid[i][j]
+                    new_r = (r + val) % k
+
+                    if i > 0:
+                        dp[i][j][new_r] = (dp[i][j][new_r] + dp[i-1][j][r]) % MOD
+                    if j > 0:
+                        dp[i][j][new_r] = (dp[i][j][new_r] + dp[i][j-1][r]) % MOD
+
+        return dp[m-1][n-1][0]
