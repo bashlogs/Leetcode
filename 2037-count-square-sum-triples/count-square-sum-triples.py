2e28ebf40fed19ed6f9a686a555f8ef95929c20e
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2025-12-08 19:30:46 +0530

    Time: 374 ms (25.27%) | Memory: 17.9 MB (11.68%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.py
+++ 2037-count-square-sum-triples/count-square-sum-triples.py
@@ -3,9 +3,9 @@ class Solution:
         count = 0
 
         for i in range(1, n+1):
-            for j in range(i+1, n+1):
-                for k in range(j+1, n+1):
-                    if (i ** 2) + (j ** 2) == (k ** 2):
-                        count += 2
+            for j in range(1, n+1):
+                c = int(sqrt(i**2 + j**2 + 1))
+                if c <= n and c**2 == i**2 + j**2:
+                    count += 1
             
         return count
