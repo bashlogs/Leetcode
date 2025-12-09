c70a36e125c476857c541cd9573f787ebfde53b7
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2025-12-09 19:42:36 +0530

    Time: 703 ms (64.41%) | Memory: 43.7 MB (37.84%) - LeetSync

---
+++ 3885-count-special-triplets/count-special-triplets.py
@@ -0,0 +1,20 @@
+class Solution:
+    def specialTriplets(self, nums: List[int]) -> int:
+        hmap1 = defaultdict(int)
+        hmap2 = Counter(nums)
+        count = 0
+        mod = 10**9 + 7
+
+        for i in range(len(nums) - 1):
+            temp = nums[i]
+            hmap2[temp] -= 1
+                
+            mul = (temp * 2) % mod
+            if hmap1[mul] > 0 and hmap2[mul] > 0:
+                count += (hmap1[mul] * hmap2[mul]) % mod
+                count %= mod
+            
+            hmap1[temp] += 1
+        
+        return count
+
