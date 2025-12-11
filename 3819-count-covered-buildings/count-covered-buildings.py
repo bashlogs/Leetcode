ec2d6602e22cb9e1d57015681803c69fcf29cf14
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2025-12-11 22:00:00 +0530

    Time: 1452 ms (6.73%) | Memory: 59.2 MB (59.66%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.py
+++ 3819-count-covered-buildings/count-covered-buildings.py
@@ -1,7 +1,7 @@
 class Solution:
     def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
         x_building = sorted(buildings, key = lambda x: (x[1], x[0]))
-        y_building = sorted(x_building, key = lambda x: (x[0]))
+        y_building = sorted(x_building, key = lambda x: (x[0], x[1]))
 
         map1 = defaultdict(int)
         count = 0
@@ -12,15 +12,15 @@ class Solution:
             
             if x_building[i][1] == x_building[i+1][1]:
                 map1[tuple(x_building[i])] += 1
-        
-        for i in range(1, len(y_building) - 1):
+            
             if y_building[i-1][0] == y_building[i][0]:
                 map1[tuple(y_building[i])] += 1
             
             if y_building[i][0] == y_building[i+1][0]:
                 map1[tuple(y_building[i])] += 1
-            
-            if map1[tuple(y_building[i])] >= 4:
+        
+        for k, v in map1.items():
+            if v >= 4:
                 count += 1
 
         return count
