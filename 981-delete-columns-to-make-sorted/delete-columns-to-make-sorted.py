2153a424322c518aec07e414c7f206470210d367
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2025-12-20 12:09:34 +0530

    Time: 100 ms (7.04%) | Memory: 18.3 MB (73.28%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.py
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.py
@@ -5,7 +5,7 @@ class Solution:
 
         for j in range(len(strs[0])):
             for i in range(1, len(strs)):
-                if strs[i-1][j] > strs[i][j]:
+                if ord(strs[i-1][j]) > ord(strs[i][j]):
                     count += 1
                     break
         
