f6d936bde86715d2bfc80f5b4e4a74a39e851bf3
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2026-01-04 12:33:19 +0530

    Time: 191 ms (60.63%), Space: 18.6 MB (87.8%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sumFourDivisors(self, nums: List[int]) -> int:
+        
+        ans = 0
+        
+        for num in nums:
+            divisors = set()
+
+            for i in range(1, int(math.sqrt(num)) + 1):
+                if num % i == 0:
+                    divisors.add(i)
+                    divisors.add(num // i)
+
+                if len(divisors) > 4:
+                    break
+
+            if len(divisors) == 4:
+                ans += sum(divisors)
+
+        return ans
