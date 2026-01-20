88e78a27c33b05da94218196cb75fe9eb4b8db07
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2026-01-20 22:08:00 +0530

    Time: 3 ms (84.56%), Space: 19.3 MB (15.44%) - LeetHub

---
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        """
+        10
+        11
+        101
+        111
+
+        """
+        ans = [0] * len(nums)
+
+        for c, num in enumerate(nums):
+            res = -1
+            d = 1
+
+            while num & d != 0:
+                res = num - d
+                d <<= 1
+            
+            ans[c] = res
+        
+        return ans
