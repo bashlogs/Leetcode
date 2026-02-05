9ead1de8b7b73708255a306a5f9c73dff267e8f1
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2026-02-05 16:02:40 +0530

    Time: 67 ms (18.83%), Space: 19.3 MB (56.05%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.py
+++ 3379-transformed-array/3379-transformed-array.py
@@ -2,13 +2,13 @@ class Solution:
     def constructTransformedArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
         result = [0] * n
+        
         for i in range(len(nums)):
-            ans = nums[i]
             if nums[i] > 0:
-                ans = nums[(i + nums[i]) % n]
+                result[i] = nums[(i + nums[i]) % n]
             elif nums[i] < 0:
-                ans = nums[(nums[i] + i) % n]
-
-            result[i] = ans
+                result[i] = nums[(nums[i] + i) % n]
+            else:
+                result[i] = nums[i]
         
         return result
