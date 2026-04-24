a4a2a719310aa001f8afa8a3e5735eb52eacb38f
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2026-04-24 11:04:41 +0530

    Time: 0 ms (100%), Space: 19.2 MB (53.9%) - LeetHub

---
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.py
@@ -0,0 +1,6 @@
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        left = moves.count("L")
+        right = moves.count("R")
+
+        return len(moves) - (min(left, right) * 2)
