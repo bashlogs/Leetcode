23dc4378e6dc8006a9fdf15bc697f33752223ff2
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2026-08-02 21:10:03 +0530

    Time: 0 ms (100%), Space: 19.2 MB (81.8%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -1,9 +1,8 @@
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
         total = 0
-        
+
         for ch in columnTitle:
-            total += (total * 25)
-            total += (ord(ch) - (ord('A') - 1))
+            total += total * 25 + (ord(ch) - (ord('A') - 1))
 
         return total
