dbd7c688ab0282d389c76883f3c7ba16e84d9eaa
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2026-08-21 22:33:28 +0530

    Time: 1772 ms (5%), Space: 77.9 MB (28.18%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        """
+
+        dict with tuples
+
+        """
+
+        graph = defaultdict(list)
+
+        for i, j, k in roads:
+            graph[i].append([j,k])
+            graph[j].append([i,k])
+        
+        visited = set()
+        queue = deque([1])
+        min_val = float('inf')
+
+        while queue:
+            curr = queue.popleft()
+            visited.add(curr)
+
+            for u, val in graph[curr]:
+                if u not in visited:
+                    min_val = min(min_val, val)
+                    queue.append(u)
+            
+        return min_val
+                
+
