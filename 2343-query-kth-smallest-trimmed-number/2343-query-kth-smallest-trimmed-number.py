978877c0c8223eddce169a65cc8050fc4340382c
Mayur Khadde <102829101+bashlogs@users.noreply.github.com>
2026-08-22 23:24:16 +0530

    Time: 377 ms (36.18%), Space: 20.4 MB (13.31%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.py
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.py
@@ -2,19 +2,18 @@ class Solution:
     def smallestTrimmedNumbers(self, nums: List[str], queries: List[List[int]]) -> List[int]:
         ans = []
         cache = {}
+        
         for k, l in queries:
-
-            trimmed_nums = []
-
             if l in cache:
-                trimmed_nums = cache[l]
+                ans.append(cache[l][k-1][1])
             else:
+                trimmed_nums = []
                 for i in range(len(nums)):
                     trimmed_nums.append((int(nums[i][len(nums[i])-l:len(nums[i])]), i))
                 
                 trimmed_nums.sort(key=lambda x: (x[0], x[1]))
                 cache[l] = trimmed_nums
             
-            ans.append(trimmed_nums[k-1][1])            
+                ans.append(trimmed_nums[k-1][1])
 
         return ans
